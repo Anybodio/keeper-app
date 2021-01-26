e196461994588efc5e406914f6974e2224f2fd32
Anybodio <73963562+Anybodio@users.noreply.github.com>
2021-01-26 09:20:52 +0500

    Map

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNote(el) {
+  return <Note key={el.key} title={el.title} content={el.content} />;
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
